209315d7eb8bd058c849d729cfd3e42085fb1f41
FuFuWarrior <akpuwarrior@gmail.com>
2022-05-19 12:00:53 +0100

    created coupon and cart models

---
+++ src/models/migrations/20220519105721-create-coupon.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Coupons', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      code: {
+        type: Sequelize.STRING
+      },
+      is_valid: {
+        type: Sequelize.BOOLEAN
+      },
+      discount_type: {
+        type: Sequelize.STRING
+      },
+      discount: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Coupons');
+  }
+};
