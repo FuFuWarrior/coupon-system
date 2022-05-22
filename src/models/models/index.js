5710bbdb76f882b017a04e4ab1c815cdda3d9c75
FuFuWarrior <akpuwarrior@gmail.com>
2022-05-22 11:49:45 +0100

    fixed bug in index.js sequelize

--- src/models/models/index.js
+++ src/models/models/index.js
@@ -4,25 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 // const env = 'production' || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(process.env.DATABASE_URL);
-// }
-
-if ( process.env.NODE_ENV === 'production'){
-  sequelize = new Sequelize(process.env.DATABASE_URL)
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 }
-
-// else{
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// }
 
 fs
   .readdirSync(__dirname)
